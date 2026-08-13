10f5592c128121cd84261a3457bb5bcc4f20a2a2
ADIWASI FASE PARADHI SUDHAR SAMITI <afpss2005@gmail.com>
2026-08-13 12:51:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,22 @@ if (donateBtn) {
         alert("🙏 Thank you for supporting AFPSS!");
     });
 }
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+setInterval(() => {
+
+    slides[current].classList.remove("active");
+
+    current++;
+
+    if(current >= slides.length){
+
+        current = 0;
+
+    }
+
+    slides[current].classList.add("active");
+
+},4000);
